4a9594d14ee746f8f9bbc32d2e275841ac6231d2
Keiji, Yoshimi <walf443@gmail.com>
2015-09-19 17:03:21 +0900

    deploy to multi server.

--- webapp/go/deploy.sh
+++ webapp/go/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-set -ex
+set -x
 
-SSH_SERVER=isucon01.asia-east1-c.isucon5-1060
 SSH_USER=isucon
 
 echo "deploy start by $USER" | ../../notify_slack.sh
-rsync -avz ./ $SSH_USER@$SSH_SERVER:/home/isucon/isucon4_final/webapp/go/
-ssh -t $SSH_USER@$SSH_SERVER /bin/bash -c "/home/isucon/isucon4_final/webapp/go/build_wrapper.sh"
-ssh -t $SSH_USER@$SSH_SERVER pkill golang-webapp
+
+for SSH_SERVER in isucon01.asia-east1-c.isucon5-1060 isucon02.asia-east1-c.isucon5-1060 isucon03.asia-east1-c.isucon5-1060
+do
+  rsync -avz ./ $SSH_USER@$SSH_SERVER:/home/isucon/isucon4_final/webapp/go/
+  ssh -t $SSH_USER@$SSH_SERVER /bin/bash -c "/home/isucon/isucon4_final/webapp/go/build_wrapper.sh"
+  ssh -t $SSH_USER@$SSH_SERVER pkill golang-webapp
+done
 
 echo "deploy finished $USER" | ../../notify_slack.sh
